c68b8c91adc7dc5ec19321019b1c3c764738ee7d
luowei <weiluo6@illinois.edu>
2018-12-11 10:19:38 -0600

    Final Project Commit Version

---
+++ app/src/main/java/com/example/luowei/finalproject/MainActivity.java
@@ -0,0 +1,105 @@
+package com.example.luowei.finalproject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Main screen for our API testing app.
+ */
+public final class MainActivity extends AppCompatActivity {
+    /** Default logging tag for messages from the main activity. */
+    private static final String TAG = "Lab12:Main";
+
+    /** Request queue for our network requests. */
+    private static RequestQueue requestQueue;
+
+    /**
+     * Run when our activity comes into view.
+     *
+     * @param savedInstanceState state that was saved by the activity last time it was paused
+     */
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set up a queue for our Volley requests
+        requestQueue = Volley.newRequestQueue(this);
+
+        // Load the main layout for our activity
+        setContentView(R.layout.activity_main);
+
+        // Attach the handler to our UI button
+        final Button startAPICall = findViewById(R.id.generate);
+        startAPICall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                Log.d(TAG, "Start API button clicked");
+                startAPICall();
+            }
+        });
+    }
+
+    /**
+     * Make an API call.
+     */
+    void startAPICall() {
+        try {
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    "http://uinames.com/api/",
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(final JSONObject response) {
+                            Log.d(TAG, response.toString());
+                            finishCall(response);
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(final VolleyError error) {
+                    Log.w(TAG, error.toString());
+                }
+            });
+            requestQueue.add(jsonObjectRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * finish call.
+     * @param response JSONObject returned by APi
+     */
+    void finishCall(final JSONObject response) {
+        final TextView textName = findViewById(R.id.name);
+        final TextView textSurname = findViewById(R.id.surname);
+        final TextView textGender = findViewById(R.id.gender);
+        final TextView textRegion = findViewById(R.id.region);
+        try {
+            String name = response.getString("name");
+            textName.setText(name);
+            String surname = response.getString("surname");
+            textSurname.setText(surname);
+            String gender = response.getString("gender");
+            textGender.setText(gender);
+            String region = response.getString("region");
+            textRegion.setText(region);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
